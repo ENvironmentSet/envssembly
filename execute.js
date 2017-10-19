93fe55e376421f8c6f118fd76d1a1565ae19512a
ENvironmentSet <sbshw1@gmail.com>
2017-10-19 23:35:36 +0900

    약간의 코드 수정 및,package.json 수정,execute.js,config.conf(실행 도우미) 추가

--- execute.js
+++ execute.js
@@ -1,4 +1,11 @@
 /**
- * Created by environmentset on 17. 10. 11.
+ * Created by environmentset on 17. 10. 19.
  */
-var VM = require ('./lib/Runner.js');
+var VM = require("./lib/Runner.js");
+var fs = require("fs");
+
+let files = JSON.parse(fs.readFileSync("config.conf",'utf8'));
+
+for (let file in files.execute) {
+    VM(files.execute[file]);
+}
